11c9fd7e5134cd197ddfbcc2110b4f3ed1ef130d
LongBeachHXC <fweinberg@live.com>
2018-02-26 12:26:13 -0800

    Facial Rec Box Testing

--- src/App.js
+++ src/App.js
@@ -41,9 +41,9 @@ class App extends Component {
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    console.log(width,height);
+    // const width = Number(image.width);
+    // const height = Number(image.height);
+    console.log(image);
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value})
