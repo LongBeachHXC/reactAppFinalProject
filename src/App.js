3f4956e17deb75d09cefbf122a0e812847fc1c88
LongBeachHXC <fweinberg@live.com>
2018-02-26 10:35:17 -0800

    Adding input change

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
   render() {
     return (
       <div className="App">
@@ -32,7 +42,7 @@ class App extends Component {
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
+          <ImageLinkForm onInputChange={this.onInputChange}/>
           {/*<FaceRecognition /> */}
 
       </div>
