ca0dc40fdb60f083d7d343a6ae5e259c2ebff23e
LongBeachHXC <fweinberg@live.com>
2018-02-26 11:00:41 -0800

    Adding Facial Recognition Component

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click')
-    app.models.predict("https://samples.clarifai.com/face-det.jpg")
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg")
       .then(
       function(response) {
         console.log(response);
