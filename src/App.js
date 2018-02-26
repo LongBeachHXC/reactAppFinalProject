443f3fc11279ed15fff7d6089b09d39bcb666612
LongBeachHXC <fweinberg@live.com>
2018-02-26 10:06:37 -0800

    Building out Particles Background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/navigation';
 import Logo from './Components/Logo/logo';
 import ImageLinkForm from './Components/Imagelinkform/imagelinkform';
 import Rank from './Components/Rank/rank';
 import './App.css';
 
+const particlesOptioins = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blue: 5
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Navigation />
-         <Logo />
-         <Rank />
-         <ImageLinkForm />
-         {/*<FaceRecognition /> */}
+          <Particles
+            params={particlesOptions}
+          />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+          {/*<FaceRecognition /> */}
 
       </div>
     );
