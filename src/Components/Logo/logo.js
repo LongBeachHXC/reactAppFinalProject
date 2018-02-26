1892ca4a3c96cf13a33f909ead2c79cbbfb65337
LongBeachHXC <fweinberg@live.com>
2018-02-26 09:22:39 -0800

    Added Surf Logo and Cleaned up

--- src/Components/Logo/logo.js
+++ src/Components/Logo/logo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './logo.css';
+import Surf from './surf.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner">
-                    👽
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3">
+                    <img alt='logo' style={{paddingTop:'5px'}}src={Surf}/>
                 </div>
             </Tilt>
         </div>
