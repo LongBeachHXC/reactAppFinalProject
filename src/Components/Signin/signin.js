6fcc618457e4839b7923fdeed5810c587668dcb4
LongBeachHXC <fweinberg@live.com>
2018-02-26 19:11:12 -0800

    onRouteChange Function Tuning

--- src/Components/Signin/signin.js
+++ src/Components/Signin/signin.js
@@ -9,7 +9,7 @@ const Signin = ({ onRouteChange }) => {
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input
                                     className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                                     type="email"
@@ -18,7 +18,7 @@ const Signin = ({ onRouteChange }) => {
                                 />
                             </div>
                             <div className="mv3">
-                                <label className="db fw6 lh-copy f6" for="password">Password</label>
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                 <input
                                     className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                                     type="password"
