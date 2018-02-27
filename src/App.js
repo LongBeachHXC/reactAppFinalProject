206a96f4cb48b43b966efbd84d4372b0322c67ec
LongBeachHXC <fweinberg@live.com>
2018-02-26 16:37:31 -0800

    onRouteChange Function Tuning

--- src/App.js
+++ src/App.js
@@ -72,8 +72,8 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
-  onRouteChange = () => {
-    this.setState({route: 'home'})
+  onRouteChange = (route) => {
+    this.setState({route: route})
   }
 
   render() {
@@ -82,7 +82,7 @@ class App extends Component {
           <Particles className='particles'
             params={particlesOptions}
           />
-          <Navigation />
+          <Navigation onRouteChange={this.onRouteChange} />
           {this.state.route === 'signin'
           ? <Signin onRouteChange={this.onRouteChange} />
           : <div>
