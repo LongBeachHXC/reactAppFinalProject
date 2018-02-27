7e436432a7262030cd5412442829f5ff7943dbcb
LongBeachHXC <fweinberg@live.com>
2018-02-26 16:55:28 -0800

    onRouteChange Function Tuning

--- src/App.js
+++ src/App.js
@@ -86,20 +86,20 @@ class App extends Component {
           <Navigation onRouteChange={this.onRouteChange} />
           {this.state.route === 'home'
             ? <div>
-              <Logo />
-              <Rank />
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
-            </div>
-            : {
+                <Logo />
+                <Rank />
+                <ImageLinkForm
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+              </div>
+            : (
               this.state.route === 'signin'
                 ? <Signin onRouteChange={this.onRouteChange}
                 : <Register onRouteChange={this.onRouteChange}
               />
-          }
+          )
 
           }
       </div>
