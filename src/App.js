860addd1f86832a3dc7c5765d1042b7e42547bc9
LongBeachHXC <fweinberg@live.com>
2018-02-26 16:33:26 -0800

    onRouteChange Function Tuning

--- src/App.js
+++ src/App.js
@@ -72,6 +72,10 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  onRouteChange = () => {
+    this.setState({route: 'home'})
+  }
+
   render() {
     return (
       <div className="App">
@@ -80,7 +84,7 @@ class App extends Component {
           />
           <Navigation />
           {this.state.route === 'signin'
-          ? <Signin />
+          ? <Signin onRouteChange={this.onRouteChange} />
           : <div>
             <Logo />
             <Rank />
