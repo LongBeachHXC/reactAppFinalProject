4ac08966e26c9a4ab582445f9d948108aeb224f3
LongBeachHXC <fweinberg@live.com>
2018-02-26 19:44:31 -0800

    onRouteChange Function Tuning

--- src/App.js
+++ src/App.js
@@ -80,7 +80,9 @@ class App extends Component {
     } else if (route ==='home') {
       this.SetState({isSignedIn: true})
     }
-    this.setState({route: route});
+    this.setState({route: route})
+    console.log(route)
+    console.log(isSignedIn);
   }
 
   render() {
