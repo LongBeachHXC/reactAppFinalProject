2f8740203913f8dac4a2a634b046aedfdbf669ab
LongBeachHXC <fweinberg@live.com>
2018-02-27 20:30:11 -0800

    Integrating with backend

--- src/Components/Register/register.js
+++ src/Components/Register/register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://10.13.113.4:3001/signin', {
+        fetch('http://10.13.113.4:3001/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
